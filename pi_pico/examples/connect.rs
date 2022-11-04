48917c3b3259af80ba761d79e5191a47669cd166
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-11-03 21:58:45 -0700

    Update pico example to use interrupts

--- pi_pico/examples/connect.rs
+++ pi_pico/examples/connect.rs
@@ -6,6 +6,8 @@ use critical_section::Mutex;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::timer::CountDown;
+use fugit::ExtU32;
 use fugit::RateExtU32;
 use panic_probe as _;
 
@@ -16,27 +18,26 @@ use bsp::hal::gpio::Interrupt::EdgeLow;
 use bsp::hal::pac::interrupt;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    gpio,
-    gpio::dynpin::DynPin,
+    gpio::{self, bank0::Gpio22, Input, dynpin::DynPin, Pin, PullUp},
     pac,
     sio::Sio,
     spi,
+    timer::Timer,
     watchdog::Watchdog,
 };
 
 use atwinc1500::wifi::Channel;
 use atwinc1500::wifi::Connection;
 use atwinc1500::Atwinc1500;
+use atwinc1500::Status;
 
 use core::cell::RefCell;
 use core::env;
 
-type Atwinc =
-    Atwinc1500<'static, spi::Spi<spi::Enabled, pac::SPI0, 8>, cortex_m::delay::Delay, DynPin>;
-type IrqPin = bsp::hal::gpio::Pin<
-    bsp::hal::gpio::bank0::Gpio22,
-    bsp::hal::gpio::Input<bsp::hal::gpio::PullUp>,
->;
+type Atwinc = Atwinc1500<spi::Spi<spi::Enabled, pac::SPI0, 8>, cortex_m::delay::Delay, DynPin>;
+type IrqPin = Pin<Gpio22, Input<PullUp>>;
+
+// Define static variables to be passed to the interrupt
 static ATWINC: Mutex<RefCell<Option<Atwinc>>> = Mutex::new(RefCell::new(None));
 static IRQ_PIN: Mutex<RefCell<Option<IrqPin>>> = Mutex::new(RefCell::new(None));
 
@@ -62,6 +63,9 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut count_down = timer.count_down();
+
     let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     let pins = bsp::Pins::new(
@@ -86,16 +90,14 @@ fn main() -> ! {
     onboard_led.set_high().unwrap();
 
     let cs: gpio::DynPin = pins.gpio17.into_push_pull_output().into();
-    let irq: bsp::hal::gpio::Pin<
-        bsp::hal::gpio::bank0::Gpio22,
-        bsp::hal::gpio::Input<bsp::hal::gpio::PullUp>,
-    > = pins.gpio22.into_pull_up_input().into();
+    let irq: IrqPin = pins.gpio22.into_pull_up_input().into();
     irq.set_interrupt_enabled(EdgeLow, true);
     let reset: gpio::DynPin = pins.gpio21.into_push_pull_output().into();
     let mut en_wake: gpio::DynPin = pins.gpio20.into_push_pull_output().into();
     en_wake.set_high().unwrap();
 
-    let atwinc1500 = Atwinc1500::new(spi, delay, cs, reset, false);
+    let mut atwinc1500 = Atwinc1500::new(spi, delay, cs, reset, false);
+    atwinc1500.initialize().unwrap();
 
     info!("Create atwinc struct");
 
@@ -104,11 +106,12 @@ fn main() -> ! {
     // Read password from environment variable
     const PASS: &[u8] = env!("PASS").as_bytes();
 
-    // Connect to the network with our connection
-    // parameters
+    // Create connection parameters
     let connection = Connection::wpa_psk(SSID, PASS, Channel::default(), 0);
 
     critical_section::with(|cs| {
+        // Store the driver and irq pin in
+        // the static variables
         ATWINC.borrow(cs).replace(Some(atwinc1500));
         IRQ_PIN.borrow(cs).replace(Some(irq));
     });
@@ -118,19 +121,27 @@ fn main() -> ! {
     }
 
     critical_section::with(|cs| {
-        if let Some(mut atwinc) = ATWINC.borrow(cs).take() {
-            atwinc.initialize().unwrap();
+        if let Some(atwinc) = ATWINC.borrow(cs).take() {
+            // Check status of atwinc
+            // it should be idle
+            info!("Status: {:?}", atwinc.get_status());
+            ATWINC.borrow(cs).replace(Some(atwinc));
         }
     });
 
     critical_section::with(|cs| {
         if let Some(mut atwinc) = ATWINC.borrow(cs).take() {
+            // Connect to the network with our connection parameters
             atwinc.connect_network(connection).unwrap();
+            ATWINC.borrow(cs).replace(Some(atwinc));
         }
     });
 
+    count_down.start(6000u32.millis());
+    let _ = nb::block!(count_down.wait());
+
     critical_section::with(|cs| {
-        if let Some(mut atwinc) = ATWINC.borrow(cs).take() {
+        if let Some(atwinc) = ATWINC.borrow(cs).take() {
             info!("{:?}", atwinc.get_status());
         }
     });
@@ -140,26 +151,25 @@ fn main() -> ! {
 
 #[interrupt]
 fn IO_IRQ_BANK0() {
-    static mut WINC: Option<Atwinc> = None;
-    static mut IRQ: Option<IrqPin> = None;
+    info!("Enter Interrupt");
+    critical_section::with(|cs| {
+        // Take the driver an interrupt request pin 
+        let winc = ATWINC.borrow(cs).take();
+        let irq = IRQ_PIN.borrow(cs).take();
 
-    if WINC.is_none() {
-        critical_section::with(|cs| {
-            *WINC = ATWINC.borrow(cs).take();
-        });
-    }
-    if IRQ.is_none() {
-        critical_section::with(|cs| {
-            *IRQ = IRQ_PIN.borrow(cs).take();
-        });
-    }
-    info!("Interrupt");
+        if let Some(mut atwinc) = winc {
+            // Handle driver events and then
+            // put it back in the static variable
+            atwinc.handle_events().unwrap();
+            ATWINC.borrow(cs).replace_with(|_| Some(atwinc));
+        }
 
-    if let Some(atwinc) = WINC {
-        atwinc.handle_events().unwrap();
-    }
-
-    if let Some(irq) = IRQ {
-        irq.clear_interrupt(EdgeLow);
-    }
+        if let Some(mut irq_pin) = irq {
+            // Clear our interrupt and 
+            // put the pin back in the static variable
+            irq_pin.clear_interrupt(EdgeLow);
+            IRQ_PIN.borrow(cs).replace_with(|_| Some(irq_pin));
+        }
+    });
+    info!("Exit interrupt");
 }
